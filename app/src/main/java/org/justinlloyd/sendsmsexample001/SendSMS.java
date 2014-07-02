81aa862504e9f1191477606a040e0f2fb2a67d76
Justin Lloyd <justin@justinlloyd.org>
2014-07-02 16:53:11 -0700

    Populate the SMS composer with a default message
    Using the putExtra functionality of an intent, tack on a message that we can send out

--- app/src/main/java/org/justinlloyd/sendsmsexample001/SendSMS.java
+++ app/src/main/java/org/justinlloyd/sendsmsexample001/SendSMS.java
@@ -53,6 +53,7 @@ public class SendSMS extends Activity {
 
         Uri smsURI = Uri.fromParts("sms", destinationTelephoneNumber, null);
         Intent smsIntent = new Intent(Intent.ACTION_VIEW, smsURI);
+        smsIntent.putExtra("sms_body", "Let's tell the world \"hello!\" from our SendSMS app");
         startActivity(smsIntent);
         Log.d(SendSMS.class.getName(), "Sent a request to open the SMS Composer activity");
         Toast.makeText(this, "Opening SMS composer", Toast.LENGTH_SHORT).show();
