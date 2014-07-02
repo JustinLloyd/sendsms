02d324008c9b94668e480bb33e07aef8dffa14c0
Justin Lloyd <justin@justinlloyd.org>
2014-07-02 16:33:49 -0700

    Add simple error checking on the telephone number
    Verify that the telephone number supplied in the edit text on the main activity is not empty

--- app/src/main/java/org/justinlloyd/sendsmsexample001/SendSMS.java
+++ app/src/main/java/org/justinlloyd/sendsmsexample001/SendSMS.java
@@ -43,6 +43,11 @@ public class SendSMS extends Activity {
     {
         Log.d(SendSMS.class.getName(), "Send SMS button clicked");
         String destinationTelephoneNumber = ((EditText)(findViewById(R.id.editTextDestinationTelephoneNumber))).getText().toString();
+        if (destinationTelephoneNumber.isEmpty())
+        {
+            return;
+        }
+
         Uri smsURI = Uri.fromParts("sms", destinationTelephoneNumber, null);
         Intent smsIntent = new Intent(Intent.ACTION_VIEW, smsURI);
         startActivity(smsIntent);
